2cd2874d2950a703db41b1281f0ed50287ec2a5e
超 <1351714924@qq.com>
2018-07-13 11:59:58 +0800

    第一次提交

---
+++ MyDemo/src/com/chao/Main.java
@@ -0,0 +1,11 @@
+package com.chao;
+
+/* I AM A PURE SHEEP
+ *                   
+ * WHAT YOU WANT TO DO                 
+ *                 
+ * FUCK AS YOU WISH                                           
+ */
+public class Main {
+    //dasdsaafafa
+}
